71aacc0a4d4b31a36b4cbf0eb451da024f3b6036
shiyi <fengling_shiyi1001@163.com>
2021-09-17 14:51:32 +0800

    feat: 两个栈实现队列的功能

---
+++ src/com/leetcode/editor/cn/YongLiangGeZhanShiXianDuiLieLcof.java
@@ -0,0 +1,87 @@
+//用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的
+//功能。(若队列中没有元素，deleteHead 操作返回 -1 ) 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：
+//["CQueue","appendTail","deleteHead","deleteHead"]
+//[[],[3],[],[]]
+//输出：[null,null,3,-1]
+// 
+//
+// 示例 2： 
+//
+// 输入：
+//["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+//[[],[],[5],[2],[],[]]
+//输出：[null,-1,null,null,5,2]
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= values <= 10000 
+// 最多会对 appendTail、deleteHead 进行 10000 次调用 
+// 
+// Related Topics 栈 设计 队列
+
+  
+package com.leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 栈 先进后出  队列 先进先出， 利用一个栈存数据 一个栈 删数据
+ * Stack  是线程安全的  效率要比LinkedList 慢
+ * @Param null
+ * @Return
+ * @Date 2021/09/16 11:38
+ * @Author FengL
+ */
+public class YongLiangGeZhanShiXianDuiLieLcof{
+    public static void main(String[] args) {
+        CQueue cQueue = new YongLiangGeZhanShiXianDuiLieLcof().new CQueue();
+      
+      }
+   //leetcode submit region begin(Prohibit modification and deletion)
+   class CQueue {
+       private Stack<Integer> stack1;
+       private Stack<Integer> stack2;
+//       private LinkedList<Integer> stack1;
+//       private LinkedList<Integer> stack2;
+
+       public CQueue() {
+           stack1 = new Stack<>();
+           stack2 = new Stack<>();
+//           stack1 = new LinkedList<>();
+//           stack2 = new LinkedList<>();
+       }
+
+       public void appendTail(int value) {
+        stack1.push(value);
+       }
+
+       public int deleteHead() {
+           if (!stack2.isEmpty()) {
+               return stack2.pop();
+           } else {
+               while (!stack1.isEmpty()) {
+                   stack2.push(stack1.pop());
+               }
+               return stack2.isEmpty() ? -1 : stack2.pop();
+           }
+       }
+   }
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
