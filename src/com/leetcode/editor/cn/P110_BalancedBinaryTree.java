afbba6690c2c5cc7bd7b4dae1cbfa24fdf7cbb28
Shiyi <958344134@qq.com>
2022-08-02 21:59:08 +0800

    平衡二叉树

---
+++ src/com/leetcode/editor/cn/P110_BalancedBinaryTree.java
@@ -0,0 +1,115 @@
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：root = []
+ * 输出：true
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10⁴ <= Node.val <= 10⁴
+ * <p>
+ * <p>
+ * Related Topics 树 深度优先搜索 二叉树 👍 1091 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+/**
+ * @Title 平衡二叉树
+ * @Date 2022-08-02 21:37:52
+ */
+
+public class P110_BalancedBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P110_BalancedBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 一颗平衡二叉树的子树 也是必然是一颗平衡二叉树
+     * 前序遍历
+     * 还有一种后续遍历
+     */
+    class Solution {
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return Math.abs(deep(root.left) - deep(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        int deep(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return Math.max(deep(root.left), deep(root.right)) + 1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
