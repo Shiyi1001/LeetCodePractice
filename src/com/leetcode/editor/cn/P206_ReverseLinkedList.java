a931fe068c5eb18685aa30cfeb5aaa977b2553d9
Shiyi <958344134@qq.com>
2022-08-15 22:30:07 +0800

    同构字符串，反转链表，存在重复元素

---
+++ src/com/leetcode/editor/cn/P206_ReverseLinkedList.java
@@ -0,0 +1,93 @@
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * <p>
+ * Related Topics 递归 链表 👍 2695 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+import com.leetcode.editor.commons.ListNode;
+
+/**
+ * @Title 反转链表
+ * @Date 2022-08-15 21:47:34
+ */
+
+public class P206_ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new P206_ReverseLinkedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    /**
+     * 双指针  pre前一个指针 cur当前指针  每次实行一个局部反转  需要一个临时变量
+     * pre cur  再往前移动
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+
+            ListNode pre = null;
+            ListNode cur = head;
+            ListNode next;
+            while (cur != null) {
+                next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/com/leetcode/editor/cn/P217_ContainsDuplicate.java
@@ -0,0 +1,67 @@
+/**
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [1,2,3,1]
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：false
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums = [1,1,1,3,3,4,3,2,4,2]
+ * 输出：true
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= nums.length <= 10⁵
+ * -10⁹ <= nums[i] <= 10⁹
+ * <p>
+ * <p>
+ * Related Topics 数组 哈希表 排序 👍 796 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Title 存在重复元素
+ * @Date 2022-08-15 22:06:44
+ */
+
+public class P217_ContainsDuplicate {
+    public static void main(String[] args) {
+        Solution solution = new P217_ContainsDuplicate().new Solution();
+        System.out.println(solution.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 4}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                if (!set.add(num)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
