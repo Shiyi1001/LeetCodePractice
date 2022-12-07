0905745d87e9478fcf9bd959ece983a78b0374c1
Shiyi <958344134@qq.com>
2022-12-07 22:13:14 +0800

    二叉树的直径

---
+++ src/com/leetcode/editor/cn/P543_DiameterOfBinaryTree.java
@@ -0,0 +1,78 @@
+/**
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 : 给定二叉树
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * <p>
+ * <p>
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ * <p>
+ * <p>
+ * <p>
+ * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+ * <p>
+ * Related Topics 树 深度优先搜索 二叉树 👍 1194 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+import com.leetcode.editor.commons.TreeNode;
+
+/**
+ * @Title 543_二叉树的直径
+ * @Date 2022-12-07 21:51:38
+ */
+
+public class P543_DiameterOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P543_DiameterOfBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        int ans;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            ans = 1;
+            depth(root);
+            return ans - 1;
+        }
+
+        //当前节点的最大节点数
+        public int depth(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+            int L = depth(node.left);
+            int R = depth(node.right);
+            //计算当前节点的深度
+            ans = Math.max(ans, L + R + 1);
+            //返回该节点为根的子树的深度
+            return Math.max(L, R) + 1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
