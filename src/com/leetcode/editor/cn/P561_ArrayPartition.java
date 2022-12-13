54d7af6be966300f82d74aec0311e2452c794ecb
Shiyi <958344134@qq.com>
2022-12-13 22:15:02 +0800

    数组拆分

---
+++ src/com/leetcode/editor/cn/P561_ArrayPartition.java
@@ -0,0 +1,69 @@
+/**
+ * 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从
+ * 1 到 n 的 min(ai, bi) 总和最大。
+ * <p>
+ * 返回该 最大总和 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [1,4,3,2]
+ * 输出：4
+ * 解释：所有可能的分法（忽略元素顺序）为：
+ * 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+ * 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+ * 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+ * 所以最大总和为 4
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [6,2,6,5,1,2]
+ * 输出：9
+ * 解释：最优的分法为 (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6
+ * = 9
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= n <= 10⁴
+ * nums.length == 2 * n
+ * -10⁴ <= nums[i] <= 10⁴
+ * <p>
+ * <p>
+ * Related Topics 贪心 数组 计数排序 排序 👍 323 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * @Title 561_数组拆分
+ * @Date 2022-12-13 22:10:15
+ */
+
+public class P561_ArrayPartition {
+    public static void main(String[] args) {
+        Solution solution = new P561_ArrayPartition().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int arrayPairSum(int[] nums) {
+            int res = 0;
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i += 2) {
+                res += nums[i];
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
