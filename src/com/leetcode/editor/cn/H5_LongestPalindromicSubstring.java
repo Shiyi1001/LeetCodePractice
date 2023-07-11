59cca57cc1c1c32135dd90528eb49a1bc22e50cf
Shiyi <958344134@qq.com>
2023-07-11 22:01:40 +0800

    feat：路径调整

---
+++ src/com/leetcode/editor/cn/H5_LongestPalindromicSubstring.java
@@ -0,0 +1,55 @@
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * <p>
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ * <p>
+ * <p>
+ * Related Topics 字符串 动态规划 👍 6593 👎 0
+ */
+
+package com.leetcode.editor.cn;
+
+/**
+ * @Title 最长回文子串
+ * @Date 2023-06-27 22:38:02
+ */
+
+public class H5_LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new H5_LongestPalindromicSubstring().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
